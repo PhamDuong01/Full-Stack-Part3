6ba84ecc12210864ebcc96ea2650d9ece4e28d35
Pham Duong <phamduong11@gmail.com>
2023-05-06 17:36:57 +0700

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
+app.use(express.static("build"));
 const morgan = require("morgan");
 
 morgan.token("body", function (req, res) {
