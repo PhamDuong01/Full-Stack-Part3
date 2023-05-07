3ee3497875a39af3423a50f3590c941234d519ab
Pham Duong <phamduong11@gmail.com>
2023-05-07 21:23:49 +0700

    3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -2,54 +2,44 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.static("build"));
+
+//setup morgan to log requests
 const morgan = require("morgan");
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
-
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+//setup cors
+const cors = require("cors");
+app.use(cors());
+
+const personService = require("./models/person");
+
+//Router
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+app.get("/api/persons", async (request, response) => {
+  const data = await personService.getAllPerson();
+  response.json(data);
 });
 
-app.get("/info", (request, response) => {
+app.get("/info", async (request, response) => {
   const time = new Date();
-  return response.send(`<p>Phonebook has info for ${persons.length} people </p>
+  let number = await personService.Person.find({}).then((person) => {
+    return person.length;
+  });
+  return response.send(`<p>Phonebook has info for ${number} people </p>
   <p>${time}</p>
   `);
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
+app.get("/api/persons/:id", async (request, response) => {
+  console.log(request.params.id);
+  const person = await personService.getPerson(request.params.id);
 
   if (person) {
     response.json(person);
@@ -58,34 +48,7 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.filter((person) => person.id !== id);
-  persons = person;
-  response.json(persons);
-});
-
-app.post("/api/persons", (request, response) => {
-  const id = Math.floor(Math.random() * 1000);
-  const data = request.body;
-  const name = data.name;
-  const number = data.number;
-  if (name !== "" && number !== "") {
-    const validname = persons.find((person) => person.name === name);
-
-    if (validname) {
-      return response.status(400).json({ error: "name must be unique" });
-    }
-    persons.push({ id, name, number });
-    response.json({ id, name, number });
-    return;
-  }
-  name === ""
-    ? response.status(400).json({ error: "missing name" })
-    : response.status(400).json({ error: "missing number" });
-  return;
-});
-
+//start server
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
---
+++ models/person.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const url = process.env.MONGODB_URI;
+
+mongoose.set("strictQuery", false);
+console.log("connecting to PhoneBook MongoDB", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to PhoneBook MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to PhoneBook MongoDB:", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+function getAllPerson() {
+  return Person.find({}).then((person) => {
+    return person;
+  });
+}
+async function getPerson(id) {
+  const searchRessult = await Person.findById(id).then((person) => {
+    return person;
+  });
+  return searchRessult;
+}
+
+function addPerson(person) {
+  const personAdd = new Person({
+    name: person.name,
+    number: person.number,
+  });
+  //   const validname = Person.find({ name: personAdd.name }).then((person) => person.name);
+
+  //   if (validname === addperson.name) {
+  //     console.log(validname);
+  //     console.log("đã có lỗi");
+  //     return;
+  //   }
+  return personAdd.save().then((result) => {
+    console.log(`added ${personAdd.name} ${personAdd.number} to phonebook`);
+    // mongoose.connection.close();
+  });
+}
+
+function updatePerson(id, person) {
+  const personUpdate = person.number;
+  Person.findById(id).then((person) => {
+    console.log(person);
+  });
+}
+
+async function deletePerson(id) {
+  Person.findByIdAndRemove(id).then((result) => {
+    console.log("deleted ", id);
+  });
+}
+
+module.exports = { Person, getAllPerson, getPerson, addPerson, updatePerson, deletePerson };
